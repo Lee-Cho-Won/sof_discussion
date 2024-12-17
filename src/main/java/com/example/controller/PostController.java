20abc9086e0e099c46b64ca8cfa5d8c829b59f67
LCW <divdivvid1@gmail.com>
2024-12-17 23:32:56 +0900

    디렉토리 구조 정비

--- src/main/java/com/example/controller/PostController.java
+++ src/main/java/com/example/controller/PostController.java
@@ -10,14 +10,16 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
 
     private final PostRepository postRepository;
 
-    @GetMapping("/posts")
+    @GetMapping
     public String listPosts(@PageableDefault(size = 10) Pageable pageable, Model model) {
         Page<Post> posts = postRepository.findAllByType(PostType.NORMAL, pageable);
         model.addAttribute("posts", posts);
